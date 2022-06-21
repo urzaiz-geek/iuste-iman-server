c74357205d73c64df1a8972bba7158203ded6a6e
urzaiz-geek <urzaizgithub@gmail.com>
2022-06-21 20:55:30 +0100

    initial project structure

---
+++ src/main/java/com/urzaizcoding/iusteimanserver/IusteimanServerApplication.java
@@ -0,0 +1,13 @@
+package com.urzaizcoding.iusteimanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IusteimanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IusteimanServerApplication.class, args);
+	}
+
+}
